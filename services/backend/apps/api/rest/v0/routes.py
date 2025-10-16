b4070b997f248c547c47d177c79ec8db35ec3951
Gakha Pitskhelauri <gakha.pitskhelauri@leverx.com>
2025-10-16 16:26:01 +0400

    chore:create views for separate app

--- services/backend/apps/api/rest/v0/routes.py
+++ services/backend/apps/api/rest/v0/routes.py
@@ -1,10 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+from functools import partial
 
-from apps.api.rest.v0.health_check.routes import health_check_router
-
+from apps.service.health_check.views import health_check_handler, LIVENESS_PROBE
+from apps.service.health_check.dto import HealthOut
 
 api_v0_router = APIRouter()
 
-api_v0_router.include_router(
-    health_check_router, tags=["Telemetry"], prefix="/health"
+api_v0_router.add_api_route(
+    path="/health/live",
+    endpoint=partial(health_check_handler, LIVENESS_PROBE),
+    response_model=HealthOut,
+    methods=["GET"],
+    summary="Probe live",
+    tags=["Telemetry"],
+    responses={
+        status.HTTP_200_OK: {
+            "description": "Indicates that service is healthy",
+        },
+        status.HTTP_503_SERVICE_UNAVAILABLE: {
+            "description": "Indicates that service is unhealthy",
+        },
+    },
 )
