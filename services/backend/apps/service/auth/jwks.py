1298641c6eefdfcb1177ea07128a228b10784af1
Gakha Pitskhelauri <gakha.pitskhelauri@leverx.com>
2025-10-16 16:43:26 +0400

    feat(auth): add jwks, jwt and dependency files

---
+++ services/backend/apps/service/auth/jwks.py
@@ -0,0 +1,31 @@
+import httpx
+import time
+from typing import Any
+from apps.config import get_config
+
+
+_cached_keys: list[dict[str, Any]] | None = None
+_cached_at: float | None = None
+_CACHE_TTL = 24 * 60 * 60 # 24 Hours. To be configured (Maybe move this variable to config file)
+
+
+async def get_jwks() -> list[dict[str, Any]]:
+    global _cached_keys, _cached_at
+
+    now =  time.time()
+
+    if _cached_keys and _cached_at and now - _cached_at < _CACHE_TTL:
+        return _cached_keys
+
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(get_config().azure_jwks_url)
+        resp.raise_for_status()
+
+        keys = resp.json().get('keys')
+        if not keys:
+            raise RuntimeError('JWKS endpoint did not return keys')
+
+        _cached_keys = keys
+        _cached_at = now
+
+        return keys
---
+++ services/backend/apps/service/auth/jwt.py
@@ -0,0 +1,50 @@
+from fastapi import HTTPException, Depends
+from fastapi.security import OAuth2PasswordBearer
+from jwt.algorithms import RSAAlgorithm
+from apps.config import get_config
+from apps.service.auth.jwks import get_jwks
+from typing import Any
+import jwt
+
+
+config = get_config()
+
+oauth2_scheme = OAuth2PasswordBearer(f"https://login.microsoftonline.com/{config.azure_client_id}/oauth2/v2.0/token")
+
+ISSUER = f"https://login.microsoftonline.com/{config.azure_tenant_id}/v2.0"
+
+
+async def get_signing_key(token_kid: str):
+    jwks = await get_jwks()
+
+    for key in jwks:
+        if key.get('kid') == token_kid:
+            return RSAAlgorithm.from_jwk(key)
+
+    raise HTTPException(401, detail="Invalid token: Unknown 'kid'")
+
+
+async def decode_jwt(token: str = Depends(oauth2_scheme)) -> dict[str, Any]:
+    header = jwt.get_unverified_header(token)
+    kid = header.get('kid')
+
+    if not kid:
+        raise HTTPException(401, detail="Token header missing 'kid'")
+
+    public_key = await get_signing_key(kid)
+
+    try:
+        payload = jwt.decode(
+            token,
+            key=public_key,
+            algorithms=["RS256"],
+            audience=config.azure_client_id,
+            issuer=ISSUER,
+        )
+        return payload
+
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(401, detail="Expired token")
+
+    except jwt.InvalidTokenError:
+        raise HTTPException(401, detail="Invalid token")
