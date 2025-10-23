af507772149e64c9803d92b31a42ec2c1884a12e
Luka Mania <luka.mania@leverx.com>
2025-10-23 09:53:44 +0400

    chore(api): specify response model for download endpoint

--- services/backend/src/api/rest/v1/download/routes.py
+++ services/backend/src/api/rest/v1/download/routes.py
@@ -2,13 +2,13 @@ from fastapi import APIRouter, Depends
 from src.api.rest.v1.paths import DOWNLOAD as DOWNLOAD_PATH
 from src.apps.documents.service import DownloadDocumentAPIService
 from src.apps.documents.dependency import get_download_service
-from src.apps.documents.dto import DownloadRequest
+from src.apps.documents.dto import DownloadRequest, DownloadResponse
 from src.apps.auth.dependency import require_groups
 from src.apps.auth.policies import DOWNLOAD_POLICY
 
 download_router = APIRouter()
 
-@download_router.post(DOWNLOAD_PATH)
+@download_router.post(DOWNLOAD_PATH, response_model=DownloadResponse)
 async def download_documents(
     request: DownloadRequest,
     group_claims: dict = require_groups(*DOWNLOAD_POLICY['groups']),
