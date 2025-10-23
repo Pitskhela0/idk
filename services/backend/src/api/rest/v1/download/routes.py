728420918070f9d8e2fc20517e2971330ad3f202
Luka Mania <luka.mania@leverx.com>
2025-10-23 09:42:14 +0400

    chroe(api): simplify download endpoint
    - Remove StreamingResponse approach
    - Provide file content in JSON body as base64 encoded string

--- services/backend/src/api/rest/v1/download/routes.py
+++ services/backend/src/api/rest/v1/download/routes.py
@@ -1,6 +1,4 @@
 from fastapi import APIRouter, Depends
-from fastapi.responses import StreamingResponse
-from io import BytesIO
 
 from src.apps.documents.service import DownloadDocumentAPIService
 from src.apps.documents.dependency import get_download_service
@@ -16,12 +14,5 @@ async def download_documents(
     group_claims: dict = require_groups(*DOWNLOAD_POLICY['groups']),
     service: DownloadDocumentAPIService = Depends(get_download_service)
 ):
-    result = await service.download(request.document_ids)
 
-    return StreamingResponse(
-        BytesIO(result.content),
-        media_type="application/zip" if result.file_name.endswith(".zip") else "application/octet-stream",
-        headers={
-            "Content-Disposition": f'attachment; filename="{result.file_name}"'
-        },
-    )
+    return await service.download(request.document_ids)
